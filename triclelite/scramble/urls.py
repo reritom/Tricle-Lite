ef1a4272dc0d327d6af52d5d125ceed9bb09bff2
reritom <reikudjinn@gmail.com>
2018-01-11 21:50:07 +0100

    uuid tools structure added, download/done flow initialised

--- triclelite/scramble/urls.py
+++ triclelite/scramble/urls.py
@@ -6,4 +6,6 @@ app_name = 'scrambler'
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'post/', views.post, name='post'),
+    url(r'download/(?P<uuid>\w+)/', views.download, name='download'),
+    url(r'done/(?P<uuid>\w+)/', views.done, name='done'),
 ]
--- triclelite/scramble/views.py
+++ triclelite/scramble/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from .scramble import scrambler
+from scramble.scramblecore import scrambler
 
 # Create your views here.
 
@@ -8,4 +8,20 @@ def home(request):
     return render(request, 'scramble/home.html')
 
 def post(request):
-    return JsonResponse({"Hello":"There"})
+    #validate_keys()
+    #valifate_files()
+    #for each: scrambler()
+    #create_transaction()
+    #return transaction uuid
+    return JsonResponse({"Post":True})
+
+def download(request, uuid):
+    #validate_uuid_request()
+    #
+    return JsonResponse({"Download":uuid})
+
+def done(request, uuid):
+    #validate_uuid_request()
+    #remove_uuid_from_media()
+    #end_transaction()
+    return JsonResponse({"Done":uuid})
