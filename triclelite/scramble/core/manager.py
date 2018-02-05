9e61be38bc16cf562b542f52eac5226de3d84b87
reritom <reikudjinn@gmail.com>
2018-02-05 22:27:19 +0100

    Scramble refacturing into class started

---
+++ triclelite/scramble/core/manager.py
@@ -0,0 +1,46 @@
+class ScramblerManager():
+    '''
+        This class handles the scrambling, unscrambling and saving of multiple images
+    '''
+    
+    def __init__(self):
+        pass
+
+    def run(self):
+        '''
+            This method processes each file passed to it
+            :param files: Multivalue dict of files
+            :param keys: Dict of k1,k2,k3 keys
+            :param mode: The mode, "Scramble" or "Unscramble"
+            :param path: Parh of the files to process
+        '''
+        pass
+
+    def save(self):
+        '''
+            This method saves the zipfile
+        '''
+        pass
+
+    def generateZip(self):
+        '''
+            This method creates a zipfile
+        '''
+        pass
+
+    def addToZip(self):
+        '''
+            This method adds a processed file to the zipfile
+        '''
+        pass
+
+    def scrambleFile(self):
+        '''
+            This method receives a file and scrambles it
+        '''
+        pass
+
+    def unscrambleFile(self):
+        '''
+            This method receives a file to unscramble
+        '''
---
+++ triclelite/scramble/core/scrambler.py
@@ -0,0 +1,31 @@
+class ScrambleObject():
+    '''
+        This class handles the [un]/scrambling an individual item.
+    '''
+    
+    def __init__(self):
+        pass
+
+    def generateSeeds(self):
+        '''
+            This method generates a seed for each key
+        '''
+        pass
+
+    def encodeArray(self):
+        '''
+            This method encodes an array for each seed
+        '''
+        pass
+
+    def scramble(self):
+        '''
+            This method scrambles the image
+        '''
+        pass
+
+    def unscramble(self):
+        '''
+            This method unscrambles the image
+        '''
+        pass
