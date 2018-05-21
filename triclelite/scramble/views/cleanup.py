d3ba693d2dd7a2e01e3f3d3b129b5c7185b24c10
reritom <reikudjinn@gmail.com>
2018-05-21 21:00:22 +0200

    Cleanup view added

--- triclelite/scramble/views/cleanup.py
+++ triclelite/scramble/views/cleanup.py
@@ -1,5 +1,18 @@
+from scramble.models.active_url import ActiveURL
+from scramble.tools import url_tools, media_tools
+from scramble.tools.response_tools import response_ok
+from scramble.tools.validation.decorators import superuser_only
+
 def cleanup(request):
     '''
         This method checks all active urls and expires accordingly.
     '''
-    pass
+
+    expired_count = 0
+
+    for url_obj in ActiveURL.objects.all():
+        if url_obj.is_expired():
+            url_tools.expire_url(url_obj.get_url())
+            expired_count += 1
+
+    return response_ok({'message': "{0} successfully removed in cleanup".format(expired_count)})
